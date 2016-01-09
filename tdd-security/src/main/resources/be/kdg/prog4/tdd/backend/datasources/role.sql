ab7853934a4a9744e70ccbc73d9287c786d0a9a3
Olivier Van Aken <oliver_va@live.be>
2016-01-09 13:02:50 +0100

    removed unnecessary resources

--- tdd-security/src/main/resources/be/kdg/prog4/tdd/backend/datasources/role.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Role (RoleType, "user_UserId") VALUES ('ROLE_CLIENT', 1);
-INSERT INTO Role (RoleType, "user_UserId") VALUES ('ROLE_CLIENT', 2);
-INSERT INTO Role (RoleType, "user_UserId") VALUES ('ROLE_CLIENT', 3);
-INSERT INTO Role (OveralRating, Rated, Degree, RoleType, "user_UserId") VALUES (0, 0, 'Bachelor', 'ROLE_REPAIRER', 4);
-INSERT INTO Role (OveralRating, Rated, Degree, RoleType, "user_UserId") VALUES (0, 0, 'Master', 'ROLE_REPAIRER', 5);
